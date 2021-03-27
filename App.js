15fc8c78055dbc5ff713b08eff9c4dd9fab91769
shagar <shagyfox133@gmail.com>
2021-03-27 19:32:34 +0530

    Major commit

--- App.js
+++ App.js
@@ -1,15 +1,29 @@
 import React, { Component } from 'react'
 import {Text,View,Image,StyleSheet} from 'react-native'
 import { Button } from 'react-native-elements'
+import ImagePicker from 'react-native-image-picker'
+import Tflite from 'tflite-react-native'
 
+const modelFile = 'models/model.tflite';
+const labelFile = 'models/labels.txt';
+
+let Tflite = new Tflite();
 
 export default class App extends Component {
 
     constructor(props) {
         super(props);
         this.state = {
-            count : 0,
+            user:false
         }
+        Tflite.loadModel({
+            model: modelFile,
+            label : labelFile
+        }, (err, res) => {
+            if (err) console.log(err);
+            else console.log(res);
+        })
+        
     }
 
     incrementCount = () => {
@@ -19,8 +33,21 @@ export default class App extends Component {
 
     render() {
         return (
-            <View style={styles.Container}>
-
+            <View containerStyle={{display:'flex',flexDirection:'column',alignItems:'center'}}>
+                <View style={styles.TitleContainer}>
+                    <Text style={styles.fontStyle}>Agro-Met</Text>
+                </View>
+                <View style={styles.Container}>
+                    <View>
+                        <Button containerStyle={{marginBottom : 25}} title='Advisory'></Button>
+                    </View>
+                    <View>
+                        <Button containerStyle={{marginBottom : 25}} title='Water Tank Availability'></Button>
+                    </View>
+                    <View>
+                        <Button containerStyle={{marginBottom : 25}} title='RainFall Analysis'></Button>
+                    </View>
+                </View>
             </View>
         )
     }
@@ -28,11 +55,15 @@ export default class App extends Component {
 
 const styles = StyleSheet.create({
     Container: {
-        flex: 1,
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        justifyContent: 'flex-end',
     },
     TitleContainer: {
-        marginTop: 20,
-        alignItems: 'center'
+        marginTop: 20,  
+        alignItems: 'center',
+        marginBottom:20
     },
     fontStyle: {
         fontSize: 30,
